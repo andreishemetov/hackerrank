bc785c325105bec139a3bc1e88157c62b2294c68
andrei.shemetau <andrei.shemetov@gmail.com>
2022-11-03 08:41:56 +0100

    Initial Commit

---
+++ hackerrank/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hackerrank
+//
+//  Created by Andrei Shemetov on 03/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
