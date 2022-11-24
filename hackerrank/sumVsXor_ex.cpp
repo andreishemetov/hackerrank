da4955997f8cba1583a14908ecb631ccdf1d8d75
Andrei Shemetau <andrei.shemetov@gmail.com>
2022-11-24 18:11:31 +0100

    sum vs xor

---
+++ hackerrank/sumVsXor_ex.cpp
@@ -0,0 +1,8 @@
+//
+//  sumVsXor_ex.cpp
+//  hackerrank
+//
+//  Created by Andrei Shemetov on 24.11.22.
+//
+
+#include "sumVsXor_ex.hpp"
---
+++ hackerrank/sumVsXor_ex.hpp
@@ -0,0 +1,13 @@
+//
+//  sumVsXor_ex.hpp
+//  hackerrank
+//
+//  Created by Andrei Shemetov on 24.11.22.
+//
+
+#ifndef sumVsXor_ex_hpp
+#define sumVsXor_ex_hpp
+
+#include <stdio.h>
+
+#endif /* sumVsXor_ex_hpp */
