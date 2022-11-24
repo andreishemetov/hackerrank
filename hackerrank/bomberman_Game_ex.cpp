5c428d7446cd5454b87f135436212226d6af2f76
Andrei Shemetau <andrei.shemetov@gmail.com>
2022-11-24 20:46:40 +0100

    boombermam start

---
+++ hackerrank/bomberman_Game_ex.cpp
@@ -0,0 +1,8 @@
+//
+//  bomberman_Game_ex.cpp
+//  hackerrank
+//
+//  Created by Andrei Shemetov on 24.11.22.
+//
+
+#include "bomberman_Game_ex.hpp"
---
+++ hackerrank/bomberman_Game_ex.hpp
@@ -0,0 +1,13 @@
+//
+//  bomberman_Game_ex.hpp
+//  hackerrank
+//
+//  Created by Andrei Shemetov on 24.11.22.
+//
+
+#ifndef bomberman_Game_ex_hpp
+#define bomberman_Game_ex_hpp
+
+#include <stdio.h>
+
+#endif /* bomberman_Game_ex_hpp */
