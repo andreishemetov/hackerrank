fba747c113ab7a4d37cea0215060d92116d2c489
andrei.shemetau <andrei.shemetov@gmail.com>
2024-06-03 13:23:28 +0200

    multithread

--- hackerrank/multiThreading.cpp
+++ hackerrank/multiThreading.cpp
@@ -9,8 +9,9 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <mutex>
 #include <curl/curl.h>
-#include <nlohmann/json.hpp>
+//#include <nlohmann/json.hpp>
 
 //https://medium.com/nerd-for-tech/hands-on-multithreading-with-c-01-overview-e29087ebeadb
 
@@ -30,44 +31,69 @@ void func2(){
     }
 }
 
+void runA(string threadName) {
+  for (int i = 0; i < 10; i++) {
+    string out = threadName + to_string(i) + "\n";
+    this_thread::sleep_for(chrono::milliseconds(1));
+    cout << out;
+  }
+}
+
+void runB(string threadName) {
+  for (int i = 100; i < 110; i++) {
+    string out = threadName + to_string(i) + "\n";
+    this_thread::sleep_for(chrono::milliseconds(2));
+    cout << out;
+  }
+}
+
 void execute(){
-    thread worker1(func1);
-    thread worker2(func2);
+//    thread worker1(func1);
+//    thread worker2(func2);
     
-    worker1.join();
-    worker2.join();
+    thread tA(runA, "A");
+    thread tB(runB, "\tB");
+    
+    tA.join();
+    tB.join();
     
     std::cout << "\n";
 }
 
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
-}
 
-std::string fetchCurrentTime() {
-    CURL *curl;
-    CURLcode res;
-    std::string readBuffer;
 
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://worldtimeapi.org/api/timezone/Japan");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
 
-        // Parse the response to extract the time
-        try {
-            auto jsonResponse = nlohmann::json::parse(readBuffer);
-            return jsonResponse["datetime"];
-        } catch(const std::exception& e) {
-            std::cerr << "JSON parse error: " << e.what() << '\n';
-            return "";
-        }
-    }
-    return "";
-}
+
+
+
+//static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+//{
+//    ((std::string*)userp)->append((char*)contents, size * nmemb);
+//    return size * nmemb;
+//}
+
+//std::string fetchCurrentTime() {
+//    CURL *curl;
+//    CURLcode res;
+//    std::string readBuffer;
+//
+//    curl = curl_easy_init();
+//    if (curl) {
+//        curl_easy_setopt(curl, CURLOPT_URL, "https://worldtimeapi.org/api/timezone/Japan");
+//        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+//        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+//        res = curl_easy_perform(curl);
+//        curl_easy_cleanup(curl);
+//
+//        // Parse the response to extract the time
+//        try {
+//            auto jsonResponse = nlohmann::json::parse(readBuffer);
+//            return jsonResponse["datetime"];
+//        } catch(const std::exception& e) {
+//            std::cerr << "JSON parse error: " << e.what() << '\n';
+//            return "";
+//        }
+//    }
+//    return "";
+//}
 
